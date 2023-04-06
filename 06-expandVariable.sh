26c5483c82e49c83b3c8ebaa08467811bd95b8ab
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2023-04-06 13:31:23 +0530

    More on variables

---
+++ 06-expandVariable.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+name="Ritesh Koushik"
+now=$(date)
+
+# The $USER is an environment/system variable which is pre-defined in every linux OS.
+# 
+echo "Hello $name"
+echo "The system time and date is:"
+echo $now
+echo "Your username is: $USER"
+
+
