a2ff1d2dd01c7bab2274022f2c23a8064552eb19
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2023-04-06 11:17:13 +0530

    Using variables effectively

---
+++ 04-triEcho.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+word="awesome"
+
+# Using variables allows us the flexibility to reduce the repetitive behaviour.
+# We cannot change the script every single time hence we use variables to modify
+# the parts of the program that are likely to change. 
+
+echo "Linux is $word"
+echo "Videogames are $word"
