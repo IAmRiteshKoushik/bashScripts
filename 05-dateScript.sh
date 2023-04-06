6efde91a8704e87a6d5fff611c5943203b6a9e6a
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2023-04-06 11:22:42 +0530

    Reference command output inside variable

---
+++ 05-dateScript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# We are trying to capture the output of a command, save it inside a variable
+# and then use it as per specification and requirement.
+#
+now=$(date)
+
+echo "The system date time and date is:"
+
+echo $now
