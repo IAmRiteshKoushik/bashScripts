779c45e5370522be5a6424ad50e6a87ce6b89d61
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2023-07-10 22:13:01 +0530

    Adding timetable bash file to system

---
+++ tt.sh
@@ -0,0 +1,10 @@
+#! /bon/bash
+
+echo "13th July - C Programming"
+echo "15th July - Physics"
+echo "17th July - Linear Algebra"
+echo "19th July - Cul Ed"
+echo "21st July - EEE"
+echo "24th July - Disc Math"
+echo "26th July - FDS"
+echo "28th July - UI Design"
