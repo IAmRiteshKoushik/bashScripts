66d0b448470d92e9fe530f08f65a235f351dd61a
Ritesh Koushik <riteshkoushik39@gmail.com>
2024-03-10 19:40:47 +0530

    New project list

---
+++ tt.sh
@@ -0,0 +1,9 @@
+#!/bin/bash/
+
+echo "List of projects:"
+echo "Proj 1 : NightwatchJS and NeutralinoJS"
+echo "Proj 2 : ACE Web Portal (backend)"
+echo "Proj 3 : iDEA Dashboard"
+echo "Proj 4 : Amrita Maps"
+echo "Proj 5 : CertiMint (NFT)"
+echo "Proj 6 : Inventory Management Sys (backend)"
